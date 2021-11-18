7900587a6bc7cdf731dffcabf223d8cd5e2219de
Giampiero Salvi <giampisalvi@gmail.com>
2021-11-18 14:28:54 +0100

    Create broad_classes.py

---
+++ broad_classes.py
@@ -0,0 +1,4 @@
+# broadClasses converts the original phonetic inventory used in NB Tale transcription to a set of broader phonetic classes that can be used for phonetic classification
+boradClasses = {
+  
+}
